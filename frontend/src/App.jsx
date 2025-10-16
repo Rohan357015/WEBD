2424e74a4229193b0066e4cbc65e70ec4fac097a
Rohan357015 <148183689+Rohan357015@users.noreply.github.com>
2025-10-16 09:36:36 +0530

    nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import "./App.css";
-import Demo from "./components/demo";
+import Demo from "./components/demo.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div>
+    <>
       <h1>Hello, World!</h1>
+      <button className="btn btn-primary">Click Me</button>
       <Demo />
-    </div>
+      <nav class="navbar navbar-default">
+     <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+     </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
+    </>
   );
 }
 
